7550e9c55d104ae1f2c5761c712e340a1524e7db
Diego Pagini <diegopaginidev@gmail.com>
2021-09-20 15:44:17 +0200

    deploy

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,13 +1,14 @@
 /** @format */
 
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
+import { useParams, useHistory } from 'react-router';
 import './Pokemon.scss';
 
 export const Pokemon = () => {
 	const params = useParams();
 	const { pokemonName } = params;
 	const [result, setResult] = useState([]);
+	const history = useHistory();
 
 	useEffect(() => {
 		fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
@@ -15,7 +16,9 @@ export const Pokemon = () => {
 			.then((data) => setResult(data));
 	}, []);
 
-	console.log(result);
+	const handleReturn = () => {
+		history.push('/');
+	};
 
 	return (
 		<section>
@@ -29,11 +32,37 @@ export const Pokemon = () => {
 						src={result.sprites?.front_default}
 						alt={result.name}
 					/>
-					{result.types?.map((move, i) => {
-						return <div key={i}>{move.type?.name}</div>;
-					})}
+					<h4 className='pokemon-types-title'>
+						{result.types?.length > 1 ? 'Types:' : 'Type:'}
+					</h4>
+					<section className='pokemon-types'>
+						{result.types?.map((move, i) => {
+							return (
+								<div className='pokemon-types-type' key={i}>
+									{move.type?.name}
+								</div>
+							);
+						})}
+					</section>
+					<h4 className='pokemon-moves-title'>
+						{result.moves?.length > 1 ? 'Moves:' : 'Move:'}
+					</h4>
+					<section className='pokemon-moves'>
+						{result.moves?.map((move, i) => {
+							return (
+								<div className='pokemon-moves-type' key={i}>
+									{move.move?.name}
+								</div>
+							);
+						})}
+					</section>
 				</div>
 			)}
+			<div className='return-container'>
+				<button type='button' className='return' onClick={handleReturn}>
+					Return
+				</button>
+			</div>
 		</section>
 	);
 };
